e6f41ddc0a49889024b32b3919296db2ec5e30ef
Samuel BELONDRADE <samuelbelondrade@outlook.fr>
2020-03-24 10:10:20 +0100

    edit script

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -1,1 +1,19 @@
-npm start
+#!/bin/bash
+
+npm start > /tmp/result.txt &
+
+echo "Launch compilation"
+pass=0;
+while [ $pass -eq 0 ]
+do
+	if [ $(cat /tmp/result.txt | grep "Compiled successfully!" | wc -l) == "1" ]; then
+		echo "ok"
+		pass=1;
+		exit 0;
+	fi
+	if [ $(cat /tmp/result.txt | grep "Compilation failed!" | wc -l) == "1" ]; then
+		echo "Error"
+		pass=2;
+		exit 1;
+	fi
+done
